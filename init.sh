37dd1932f3878429bd47ed89108f4acb69d77ad2
Fernando-Zzs <65518148+Fernando-Zzs@users.noreply.github.com>
2024-06-26 15:55:37 +0000

    容器启动初始化脚本

---
+++ init.sh
@@ -0,0 +1,8 @@
+git config --global user.name "Fernando-Zzs"
+git config --global user.email "65518148+Fernando-Zzs@users.noreply.github.com"
+
+code --install-extension ms-python.python
+code --install-extension ms-toolsai.jupyter
+code --install-extension GitHub.copilot
+
+cd /openbayes/input/input0/MiniChinesePhi3
